154a0d820b2b2bcd0aa1653a3f1faf54a920edac
lnardon <lucaslnardon@gmail.com>
2020-10-07 00:41:28 -0300

    ✨ Created module to create vanilla web stack files

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 import fullVanillaStack from "./modules/fullVanillaStack.mjs";
 
 console.clear();
 
 switch (process.argv.slice(2)[0]) {
   case "full-vanilla-stack":
-    fullVanillaStack();
+    fullVanillaStack(process.argv.slice(2)[1]);
     break;
   default:
-    console.error("Sorry, template not found");
+    console.error("Sorry, boilerplate not found");
 }
--- modules/fullVanillaStack.mjs
+++ modules/fullVanillaStack.mjs
@@ -1,3 +1,19 @@
-export default function fullVanillaStack() {
-  console.log("Hello");
+import fs from "fs";
+
+export default function fullVanillaStack(folderName) {
+  fs.mkdir(`./${folderName}`, (err) => {
+    if (err) throw err;
+  });
+
+  fs.writeFile(`./${folderName}/index.html`, "Hello content!", (err) => {
+    if (err) throw err;
+  });
+
+  fs.writeFile(`./${folderName}/styles.css`, "", (err) => {
+    if (err) throw err;
+  });
+
+  fs.writeFile(`./${folderName}/index.js`, "", (err) => {
+    if (err) throw err;
+  });
 }
