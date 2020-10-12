0744719ffef6ae75fda013180c3d4e505eda31cf
lnardon <lucaslnardon@gmail.com>
2020-10-11 21:59:16 -0300

    ✨ Added react basic project template

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fullVanillaStack from "./modules/fullVanillaStack.mjs";
 import nodeExpress from "./modules/nodeExpress.mjs";
 import webSass from "./modules/webSass.mjs";
 import reactComponent from "./modules/reactComponent.mjs";
+import reactBasic from "./modules/reactBasic.mjs";
 
 switch (process.argv.slice(2)[0]) {
   case "vanilla-web":
@@ -13,6 +14,9 @@ switch (process.argv.slice(2)[0]) {
     break;
   case "web-sass":
     webSass(process.argv.slice(2)[1]);
+    break;
+  case "react-basic":
+    reactBasic(process.argv.slice(2)[1]);
     break;
   case "react-component":
     reactComponent(process.argv.slice(2)[1]);
